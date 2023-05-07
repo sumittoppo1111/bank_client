a76b913d0d3c7c6185d5bff33290f2ab677e1e80
sumit <sumittoppo1111.com>
2023-05-08 00:46:53 +0530

    g

--- src/components/customerRecord/member.js
+++ src/components/customerRecord/member.js
@@ -6,7 +6,7 @@ import {useState,useEffect} from "react";
 const Member=()=>
 {
   
-    mem();
+    
 
     const[getpersonal,setpersonal]=useState({});
     const[getfinancial,setfinancial]=useState({});
